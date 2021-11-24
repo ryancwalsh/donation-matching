129a5129df0530fa7d5532cdf47e337bf19f7dd6
Ryan Walsh <rcwalsh@gmail.com>
2021-11-24 14:19:42 -0500

    using u128, logging

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -1,4 +1,9 @@
-import { storage, Context } from 'near-sdk-as';
+import { storage, Context, u128, logging } from 'near-sdk-as';
+
+/**
+ * Account IDs in NEAR are just strings. https://github.com/Learn-NEAR/NCD.L1.sample--meme-museum/blob/8c5d025d363f89fdcc7335d58d61a8e3307cd95a/src/utils.ts#L34
+ */
+export type AccountId = string;
 
 const commitments: any = {
   // TODO: Everywhere that touches this placeholder var needs to be updated to use NEAR. Maybe https://docs.near.org/docs/concepts/data-storage#persistentmap
@@ -8,25 +13,30 @@ const commitments: any = {
   },
 };
 
-export function offerMatchingFunds(recipient: string, amount: number): string {
-  const total = Context.predecessor in commitments[recipient] ? commitments[recipient][Context.predecessor] + amount : amount; // TODO use exact-math?
+export function offerMatchingFunds(recipient: AccountId, amount: u128): string {
+  const total = Context.predecessor in commitments[recipient] ? u128.add(commitments[recipient][Context.predecessor], amount) : amount;
   commitments[recipient][Context.predecessor] = total;
-  return `${Context.predecessor} is now committed to match donations to ${recipient} up to a maximum of ${total}`;
+  const result = `${Context.predecessor} is now committed to match donations to ${recipient} up to a maximum of ${total}`;
+  logging.log(result);
+  return result;
 }
 
-export function rescindMatchingFunds(recipient: string, amount: number): string {
+export function rescindMatchingFunds(recipient: AccountId, amount: u128): string {
   const total = commitments[recipient][Context.predecessor];
+  let result;
   if (amount >= total) {
     delete commitments[recipient][Context.predecessor];
-    return `${Context.predecessor} is not matching donations to ${recipient} anymore`;
+    result = `${Context.predecessor} is not matching donations to ${recipient} anymore`;
   } else {
-    commitments[recipient][Context.predecessor] = commitments[recipient][Context.predecessor] - amount; // TODO use exact-math?
-    return `${Context.predecessor} rescinded ${amount} and so is now only committed to match donations to ${recipient} up to a maximum of ${total}`;
+    commitments[recipient][Context.predecessor] = u128.sub(commitments[recipient][Context.predecessor], amount);
+    result = `${Context.predecessor} rescinded ${amount} and so is now only committed to match donations to ${recipient} up to a maximum of ${total}`;
   }
+  logging.log(result);
+  return result;
 }
 
-function transfer(sender: string, recipient: string, amount: number): string {
-  // TODO transfer funds
+function transfer(sender: AccountId, recipient: AccountId, amount: u128): string {
+  // TODO transfer funds!
   /*
   // https://github.com/Learn-NEAR/NCD.L1.sample--thanks/blob/bfe073b572cce35f0a9748a7d4851c2cfa5f09b9/src/thanks/assembly/index.ts#L56
   this.assert_owner();
@@ -39,19 +49,23 @@ function transfer(sender: string, recipient: string, amount: number): string {
   // transfer earnings to owner then confirm transfer complete
   const promise = to_owner.transfer(this.contributions.received);
   promise.then(to_self).function_call('on_transfer_complete', '{}', u128.Zero, XCC_GAS);*/
-  return `${sender} donated ${amount} to ${recipient}`;
+  const result = `${sender} donated ${amount} to ${recipient}`;
+  logging.log(result);
+  return result;
 }
 
-export function donate(recipient: string, amount: number): string {
-  const messages = [];
+export function donate(recipient: AccountId, amount: u128): string {
+  const messages: string[] = [];
   const mainDonationMessage = transfer(Context.predecessor, recipient, amount);
   messages.push(mainDonationMessage);
 
   Object.keys(commitments[recipient]).forEach((matcher: string) => {
-    const matchedAmount = Math.min(amount, commitments[recipient][matcher]); // TODO use exact-math?
+    const matchedAmount: u128 = Math.min(amount, commitments[recipient][matcher]);
     const message = transfer(matcher, recipient, matchedAmount);
     messages.push(message);
   });
 
-  return messages.join(' ');
+  const result = messages.join(' ');
+  logging.log(result);
+  return result;
 }
