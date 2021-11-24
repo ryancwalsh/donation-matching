96ba4e99e32353f0e8d64c093dc45400ce91e872
Ryan Walsh <rcwalsh@gmail.com>
2021-11-24 16:15:52 -0500

    working towards escrow (but transferFromEscrow is still wrong)

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -1,5 +1,7 @@
 import { PersistentUnorderedMap, Context, u128, logging, ContractPromise, ContractPromiseBatch } from 'near-sdk-as';
 
+// TODO: Write tests for everything in this file.
+
 const XCC_GAS = 20000000000000; // https://github.com/Learn-NEAR/NCD.L1.sample--meme-museum/blob/8c5d025d363f89fdcc7335d58d61a8e3307cd95a/src/utils.ts#L15
 
 /**
@@ -22,23 +24,25 @@ const commitments = new PersistentUnorderedMap<AccountId, MatcherAccountIdCommit
 }*/
 
 export function offerMatchingFunds(recipient: AccountId, amount: u128): string {
-  const { sender } = Context;
-  // TODO: Transfer amount to escrow. Then probably the rest of this function should be moved to a callback.
-  simpleTransfer(recipient, amount);
+  const escrow = Context.contractName;
+  const matcher = Context.sender;
+  // Transfer amount to escrow.
+  simpleTransfer(escrow, amount); // Funds go from matcher to contractName (a.k.a. "self" or "escrow").
+  // TODO: Probably the rest of this function should be moved to a callback.
   let total = amount;
   if (commitments.contains(recipient)) {
     const matchersForThisRecipient = commitments.getSome(recipient);
-    if (matchersForThisRecipient.contains(sender)) {
-      const existingCommitment = matchersForThisRecipient.getSome(sender);
+    if (matchersForThisRecipient.contains(matcher)) {
+      const existingCommitment = matchersForThisRecipient.getSome(matcher);
       total = u128.add(existingCommitment, amount);
     }
-    matchersForThisRecipient.set(sender, total);
+    matchersForThisRecipient.set(matcher, total);
   } else {
-    const matcherAccountIdCommitmentAmountMap = new PersistentUnorderedMap<AccountId, u128>('todo');
-    matcherAccountIdCommitmentAmountMap.set(sender, amount);
+    const matcherAccountIdCommitmentAmountMap = new PersistentUnorderedMap<AccountId, u128>(`matcherAccountIdCommitmentAmountMap_${matcher}`);
+    matcherAccountIdCommitmentAmountMap.set(matcher, amount);
     commitments.set(recipient, matcherAccountIdCommitmentAmountMap);
   }
-  const result = `${sender} is now committed to match donations to ${recipient} up to a maximum of ${total}`;
+  const result = `${matcher} is now committed to match donations to ${recipient} up to a maximum of ${total}`;
   logging.log(result);
   return result;
 }
@@ -49,21 +53,21 @@ function min(a: u128, b: u128): u128 {
 }
 
 export function rescindMatchingFunds(recipient: AccountId, amount: u128): string {
-  const { sender } = Context;
+  const matcher = Context.sender;
   // Should fail unless recipient exists and the funds previously committed by this matcher to this recipient are GTE this amount to rescind.
-  // TODO: Transfer amountToRescind back from escrow to matcher. Then probably the rest of this function should be moved to a callback.
-  transferFromEscrow(sender, amount);
+  transferFromEscrow(matcher, amount); // Funds go from escrow back to the matcher.
+  // TODO: Probably the rest of this function should be moved to a callback.
   const matchersForThisRecipient = commitments.getSome(recipient);
-  const total = matchersForThisRecipient.getSome(sender);
+  const total = matchersForThisRecipient.getSome(matcher);
   let amountToRescind = amount;
-  let result;
+  let result: string;
   if (amount >= total) {
     amountToRescind = total;
-    matchersForThisRecipient.delete(sender);
-    result = `${sender} is not matching donations to ${recipient} anymore`;
+    matchersForThisRecipient.delete(matcher);
+    result = `${matcher} is not matching donations to ${recipient} anymore`;
   } else {
-    matchersForThisRecipient.set(sender, u128.sub(total, amountToRescind));
-    result = `${sender} rescinded ${amountToRescind} and so is now only committed to match donations to ${recipient} up to a maximum of ${total}`;
+    matchersForThisRecipient.set(matcher, u128.sub(total, amountToRescind));
+    result = `${matcher} rescinded ${amountToRescind} and so is now only committed to match donations to ${recipient} up to a maximum of ${total}`;
   }
   logging.log(result);
   return result;
@@ -98,35 +102,38 @@ function simpleTransfer(recipient: AccountId, amount: u128): ContractPromiseBatc
 
 function transferFromEscrow(destinationAccount: AccountId, amount: u128): ContractPromiseBatch {
   const toDestinationAccount = ContractPromiseBatch.create(destinationAccount);
-  return toDestinationAccount.transfer(amount); // TODO: CRITICAL! How can it come FROM the escrow account instead?
+  return toDestinationAccount.transfer(amount); // TODO: CRITICAL! How can it come FROM the escrow account instead? https://github.com/near-examples/cross-contract-calls
 }
 
-function sendMatchingDonation(sender: AccountId, recipient: AccountId, amount: u128): string {
-  const transferPromise = transferFromEscrow(recipient, amount);
+function sendMatchingDonation(matcher: AccountId, recipient: AccountId, amount: u128, matchersForThisRecipient: MatcherAccountIdCommitmentAmountMap): string {
+  const escrow = Context.contractName;
+  const remainingCommitment: u128 = matchersForThisRecipient.getSome(matcher);
+  const matchedAmount: u128 = min(amount, remainingCommitment);
+  const transferPromise = transferFromEscrow(recipient, matchedAmount);
 
   // https://github.com/Learn-NEAR/NCD.L1.sample--thanks/blob/bfe073b572cce35f0a9748a7d4851c2cfa5f09b9/src/thanks/assembly/index.ts#L56
-  const to_self = Context.contractName;
-  transferPromise.then(to_self).function_call('onTransferComplete', '{}', u128.Zero, XCC_GAS); // TODO: Learn what this means and whether it is correct.
-  const result = `${sender} sent a matching donation of ${amount} to ${recipient}`;
+
+  transferPromise.then(escrow).function_call('onTransferComplete', '{}', u128.Zero, XCC_GAS); // TODO: Learn what this means and whether it is correct.
+  const result = `${matcher} sent a matching donation of ${matchedAmount} to ${recipient}`;
   logging.log(result);
   return result;
 }
 
 export function donate(recipient: AccountId, amount: u128): string {
   assert(amount > u128.Zero, '`amount` must be > 0');
-  const { sender } = Context;
+  const sender = Context.sender;
   const matchersForThisRecipient = commitments.getSome(recipient);
   const messages: string[] = [];
   simpleTransfer(recipient, amount);
   // TODO: Assert that the simpleTransfer succeeded.
   const mainDonationMessage = `${sender} donated ${amount} to ${recipient}`;
   messages.push(mainDonationMessage);
-  matchersForThisRecipient.keys().forEach((matcher: string) => {
-    const remainingCommitment = matchersForThisRecipient.getSome(matcher);
-    const matchedAmount: u128 = min(amount, remainingCommitment);
-    const message = sendMatchingDonation(matcher, recipient, matchedAmount);
+  const matcherKeysForThisRecipient = matchersForThisRecipient.keys();
+  for (let i = 0; i < matcherKeysForThisRecipient.length; i += 1) {
+    const matcher = matcherKeysForThisRecipient[i];
+    const message = sendMatchingDonation(matcher, recipient, amount, matchersForThisRecipient);
     messages.push(message);
-  });
+  }
   const result = messages.join(' ');
   logging.log(result);
   return result;
